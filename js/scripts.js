cc7a0182bc11dd91d1386faafdb66252b55e3faa
Rafiqul Islam <rafiqcoder@gmail.com>
2025-05-21 01:50:09 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// console.log("hello word");
+// alert("hello world");
+// comments
+
+/* dfaf
+dfdfa
+
+adfdsf
+
+
+
+dfsadff
+*/
+
+// console.log(document.getElementById("blue_box").innerHTML);
+
+// document.getElementById("blue_box").innerHTML =
+//   "Rafiqul will be the new content of the element.";
+
+// var table = " chayerkap, kolom, marker, paper";
+
+// table = " amar laptop, amar mouse , keyboard";
+
+// console.log(table);
+
+// var chayerKap = "Pani";
+
+// console.log(chayerKap);
+
+// var table;
+
+// table = "laptop";
+
+// console.log(table);
+var userName = "rafiqul";
+
+{
+  const userName3 = "shafiqul ";
+  //   userName = "shafiqul";
+  console.log(userName3);
+}
+
+
+let roll = '56';
+
