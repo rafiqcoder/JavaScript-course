ecaed363a6fcf759dbc1fcf628ad3b21975306f8
Rafiqul Islam <rafiqcoder@gmail.com>
2025-07-10 18:49:33 +0600

    added react prequisites

--- js/scripts.js
+++ js/scripts.js
@@ -235,9 +235,6 @@ dfsadff
 
 // console.log(employe.designation);
 
-
-
-
 // var courses = [
 //     {
 //         'title': 'Learn code in Python 3',
@@ -259,13 +256,11 @@ dfsadff
 //         'students': 18560,
 //         'categories': ['productivity', 'business']
 //     }
-    
-// ];
 
+// ];
 
 // courses[1].categories[1] = "writer";
 // console.log(courses[1].categories[1]); // Output: writer
-
 
 // var student = {
 //   firstName: 20,
@@ -275,49 +270,333 @@ dfsadff
 //   },
 // };
 
-
 // console.log(student.fullName());
-
 
 // 30, 20, 40
 
-
-
 // 20, c , 30
-
 
 // function calcAverage (c){
 
 //     const average = (20 + c + 30)/3;
 
-
-
-
 //     return average
 
 // }
-
 
 // console.log(calcAverage(30))
 // console.log(calcAverage(20))
 // console.log(calcAverage(40))
 
-var course = {
-    'title': "Learn to Code in Python 3",
-    'categories': ['programming', 'technology', 'python'],
-    '5_stars_reviews': 420,
-    '4_stars_reviews': 80,
-    '3_stars_reviews': 33,
-    '2_stars_reviews': 20,
-    '1_stars_reviews': 4,
-    getPercentage : function (){
-        //totasl stars / five starts 
-        //results * 100
-        return // sum of all stars and percentage of fivestars
-    }
-}
+// var course = {
+//     'title': "Learn to Code in Python 3",
+//     'categories': ['programming', 'technology', 'python'],
+//     '5_stars_reviews': 420,
+//     '4_stars_reviews': 80,
+//     '3_stars_reviews': 33,
+//     '2_stars_reviews': 20,
+//     '1_stars_reviews': 4,
+//     getPercentage : function (){
+//         //totasl stars / five starts
+//         //results * 100
+//         return // sum of all stars and percentage of fivestars
+//     }
+// }
 
-document.getElementById("color_button").onclick = function () {
-  this.style["background-color"] = "purple";
-  this.style.transform = "translateX(100px)";
+// document.getElementById("color_button").onclick = function () {
+//   this.style["background-color"] = "purple";
+//   this.style.transform = "translateX(100px)";
+// };
+
+// document.getElementById("click-me").onclick = function () {
+//   this.style.backgroundColor = "red";
+//   this.style.transform = "translateY(-50px)";
+// };
+
+// document.onkeydown = function (event) {
+//   const pressedKey = event.keyCode;
+//   // alert("You pressed the key with code: " + pressedKey);
+//   if (pressedKey === 80) {
+//     alert("You pressed the 'P' key. And want to print");
+//     window.print();
+//   }
+// };
+
+// const element = document.getElementsByClassName("box").style;
+// const element2 = document.getElementById("boxId");
+
+// // function myFunction() {
+// //   element.style.backgroundColor = "red";
+// //   element.style.transform = "translateY(50px)";
+// // }
+// var elements = document.getElementsByTagName("p");
+// console.log((elements[0].style.backgroundColor = "red"));
+
+// const date = new Date();
+
+// // let function =" this is function";
+
+// // var firstName =
+
+// let firstName = "Rafiqul";
+
+// const lastName = "islam";
+
+// const fulName = 'my name is "Rafiqul" Islam';
+
+// console.log(fulName);
+
+// // toString()
+// //parseFloat()
+// //parseInt()
+
+// // console.log(10 !== 10);// false
+
+// var orderValue = 2800;
+
+// if (orderValue <= 1000) {
+//   console.log("You are eligible to finance your order.");
+// } else {
+//   console.log("Finance options are not available for orders below US$1000");
+// }
+
+// let myName = "rafiqul";
+
+// button.onclick = function () {
+//     myName = null;
+// }
+
+// const add = (a, b) => a + b;
+
+// const obj = {
+//     firstName: "Rafiqul",
+//   add: (a, b) =>{
+//     return a + b;
+//     },
+//   lastName: "Islam",
+// };
+
+// obj.add(2, 3); // 5
+
+// setTimeout(function () {
+//   console.log("Executed after 1 second");
+// }, 1000);
+
+// // Arrow version
+// setTimeout(() => console.log("Arrow style"), 1000);
+
+// var courses = [
+//   {
+//     title: "Learn code in Python 3",
+//     reviews: 6802,
+//     students: 130129,
+//     categories: ["programming", "technology"],
+//   },
+
+//   {
+//     title: "Learn PHP -  Beginner to Advanced",
+//     reviews: 1204,
+//     students: 30521,
+//     categories: ["web development", "programming"],
+//   },
+
+//   {
+//     title: "Learn Microsoft Excel 2020",
+//     reviews: 4209,
+//     students: 18560,
+//     categories: ["productivity", "business"],
+//   },
+// ];
+
+// console.log(courses[0].categories[0]= "python");
+// var course = {
+//   title: "Learn to Code in Python 3",
+//   categories: ["programming", "technology", "python"],
+//   "5_stars_reviews": 420,
+//   "4_stars_reviews": 80,
+//   "3_stars_reviews": 33,
+//   "2_stars_reviews": 20,
+//   "1_stars_reviews": 4,
+//   getPercentage: function () {
+//     const sumTotal =
+//       this["5_stars_reviews"] +
+//       this["4_stars_reviews"] +
+//       this["3_stars_reviews"] +
+//       this["2_stars_reviews"] +
+//       this["1_stars_reviews"];
+//     const fiveStarsPercentage = (this["5_stars_reviews"] / sumTotal) * 100;
+
+//     return fiveStarsPercentage;
+//   },
+// };
+
+// let presentageResult = course.getPercentage(); // Output: 75.63636363636363
+
+// let roundedPersentage = Math.round(presentageResult);
+// document.getElementById("reviews_5_stars").innerHTML = roundedPersentage + "%";
+
+// const students = ["Rahim", "shofiq", "jabbar", "karim", "hasan"];
+
+// for (var a = 0; a < 7; a += 1) {
+//   console.log(students[a]);
+// }
+
+// var car = {
+//   Year: 2018,
+//   Model: "Evoke",
+//   Manufacturer: "Land Rover",
+//   FuelType: "Diesel",
+// };
+
+// for (var prop in car) {
+//   console.log(prop + ": " + car[prop]);
+// }
+
+// let myArray = document.getElementsByClassName("example");
+
+// for (let i = 0; i < 3; i++) {
+//   const div = document.getElementById("pushElements");
+
+//   div.innerHTML += `<li style="font-size:20px"> SOme thing defferent </li>`;
+// }
+
+// const colors = ["red", "green", "blue"];
+
+// //traditional way
+
+// colors.forEach((eachValue, index) => {
+
+//   console.log(`Color  ${eachValue}`);
+// });
+// const numbers = [1, 2, 3];
+
+// const newNumbersArray = numbers.map((num,i)=>{
+//   return `position: ${i}, value: ${num * 2}`;
+// })
+
+// console.log(newNumbersArray); // [2, 4, 6]
+
+// const ages = [12, 17, 20, 30];
+// const adults = ages.filter((age) => age >= 18);
+// console.log(adults); // [20, 30]
+
+// const scores = [10, 20, 30, 40, 60, 80];
+// const total = scores.reduce((prev, curr) => prev + curr, 0);
+// console.log(total); // 60
+
+// const users = [
+//   { name: "Tom", active: false, id: 1 },
+//   { name: "Lucy", active: true, id: 2 },
+//   { name: "John", active: true, id: 3 }, 3
+//   { name: "Doe", active: true, id: 4 },
+// ];
+
+// const activeUsers = users.find((user) => user.active === true);
+
+// console.log(activeUsers); // { name: 'Lucy', active: true }
+
+// const words = ["hello", "world"];
+// const chars = words.flatMap((word) => word.split(""));
+// console.log(chars);
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("Hello!");
+// }
+
+// var students = ["Peter", "Mary", "Joseph", "John", "Charles"];
+// // console.log(students.length); // Output: 5
+
+// for (var a = 0; a < students.length; a++) {
+//     // console.log(a);
+//   console.log(students[a]);
+// }
+
+// var car = {
+//   Year: 2018,
+//   Model: "Evoke",
+//   Manufacturer: "Land Rover",
+//   FuelType: "Diesel",
+// };
+
+// // console.log(car["Year"]);
+
+// // for in loop
+// for (var key in car) {
+//   console.log(car[key]);
+// }
+
+// const quantity = 3;
+
+// const products = [10, 200, 300, 499];
+
+// const priceWithQuantity = products.map(
+//   (eachProductPrice, i) => eachProductPrice * quantity
+// );
+
+// console.log(priceWithQuantity); // [0, 600, 900, 1497 ] grandTotal = reduce =
+
+// const grandTotal = priceWithQuantity.reduce((sumResult, currentPrice) => sumResult + currentPrice,0);
+
+// console.log(grandTotal); // 3027
+
+// const products = [
+//   { category: "shutki", price: 100, name: "dried fish", id: 1 },
+//   { category: "fish", price: 200, name: "fresh fish", id: 2 },
+//   { category: "shutki", price: 300, name: "beef", id: 3 },
+//   { category: "vegetable", price: 400, name: "carrot", id: 4 },
+// ];
+
+// const filteredProducts = products.find((a) => a.id == 4);
+// console.log(filteredProducts.name);
+
+// document.getElementById("show_option").onclick = function () {
+//   var selectField = document.getElementById("options");
+//   //   console.log();
+
+//   var selectedOption = selectField.options.selectedIndex;
+//   console.log("selectedIndex", selectedOption);
+
+//   var selectedValue = selectField.options[selectedOption].innerHTML;
+//   console.log(selectedValue);
+
+//   document.getElementById("selected_option").innerHTML = selectedValue;
+// };
+
+// document.getElementById("show_radio").onclick = function () {
+//   var radio = document.getElementsByName("gender");
+//   console.log(radio[1].value);
+
+//   var radio_selected;
+
+//   //   console.log(radio[0].checked);
+
+//   for (var a = 0; a < radio.length; a++) {
+//     if (radio[a].checked) {
+//       radio_selected = radio[a].value;
+//     }
+//   }
+
+//   document.getElementById("selected_radio").innerHTML = radio_selected;
+// };
+
+// const showBtn = document.getElementById("show_Input_value");
+
+// showBtn.onclick = function () {
+//   var inputValue = document.getElementById("getValue").value;
+
+//   console.log(typeof inputValue);
+// };
+
+document.getElementById("show_check").onclick = function () {
+  var check = document.getElementsByName("interest");
+
+  document.getElementById("selected_check").innerHTML = "";
+  console.log("clicked", check);
+
+  for (var b = 0; b < check.length; b++) {
+    if (check[b].checked) {
+      document.getElementById("selected_check").innerHTML +=
+        "<li>" + check[b].value + "</li>";
+    }
+  }
 };
